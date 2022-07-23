7337caba05d90af7eaaadfe37a2563346c631dd4
jsingh802 <jskrt655@gmail.com>
2022-07-23 14:32:10 -0400

    Updated scripts,templates and parameters

---
+++ IND7/checkpoint1.sh
@@ -0,0 +1,28 @@
+﻿#!/bin/bash
+
+# Creating Checkpoint1 Resources with bash
+echo "--------------------------------"
+echo "Loadign the variables"
+source ./config.sh
+echo "Student Deafault Resource Group $RG_Name"
+echo "--------------------------------"
+echo 
+
+echo "--------------------------------"
+echo "Creating VNETs"
+echo "--------------------------------"
+echo 
+source ./vnet_create.sh
+
+echo "--------------------------------"
+echo "Creating Route Tables"
+echo "--------------------------------"
+echo 
+# ---> Your Code in here
+
+echo "--------------------------------"
+echo "Creating DevTest Lab"
+echo "--------------------------------"
+echo 
+# ---> Your Code in here
+
---
+++ IND7/config.sh
@@ -0,0 +1,19 @@
+# Creating Checkpoint2 Resources with bash
+
+# Variable block
+Location="Canada East"
+RG_Name="Student-RG-634460"
+Student_vnet="Student-634460-vnet"
+Router_vnet="Router-71"
+Server_vnet="Server-71"
+SubNet="SN1"
+RT_Name="RT-71"
+Client_SN="Virtual-Desktop-Client"
+Virtual_Appliance="192.168.71.36"
+Server_SN1="172.17.71.32/27"
+Virtual_Desktop="10.43.156.0/24"
+DevTest_Lab="CSN500-71"
+VM_Win_Client="WC-71"
+VM_Win_Server="WS-71"
+VM_Lnx_Router="LR-71"
+VM_Lnx_Server="LS-71"
